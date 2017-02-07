32d42d561ddaf844c05b8679e5906ab6d2e54a96
Sarah Rosanna Busch <annarose0113@gmail.com>
2017-02-07 08:17:12 -0800

    initial commit

---
+++ scripts.js
@@ -0,0 +1,6 @@
+document.createElement('subTitle'); //required for IE9 and earlier
+
+document.getElementsByTagName('header')[0].innerHTML =
+"<h1 id='title'> Website Essentials </h1>" +
+"<subTitle> by Sarah Rosanna Busch </subTitle>"
+;
